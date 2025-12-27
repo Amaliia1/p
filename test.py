2825b75180c032bed90ce5fbc53bd1beffa13df7
amaliia <amaliiaarseienko@gmail.com>
2025-12-27 13:14:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-def login(username, password):
-    try:
-        assert username == "admin" and password == "1234", "Невірне ім'я користувача або пароль"
-        print("Вхід виконано успішно")
-    except AssertionError as e:
-        print(e)
 
-user = input("Введіть ім'я користувача: ")
-pwd = input("Введіть пароль: ")
-
-login(user, pwd)
